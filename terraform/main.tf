e927407d58056aabcd1fec8a2ed1d12bd2673e00
adityaoncloud <adityaoncloud25@gmail.com>
2025-07-15 10:49:45 +0530

    updated CICD

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,25 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+resource "aws_iam_role_policy" "lambda_stepfn_policy" {
+  name = "lambda-stepfn-exec"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "states:StartExecution"
+        ],
+        Resource = "arn:aws:states:ap-south-1:879112120115:stateMachine:order-orchestrator"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
@@ -186,4 +205,9 @@ resource "aws_cloudwatch_metric_alarm" "dlq_alert" {
   dimensions = {
     QueueName = "order_dlq"
   }
+}
+
+module "cicd" {
+  source = "./cicd"
+  github_token = var.github_token  # Make sure this var is defined in root-level `variables.tf`
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
+
+variable "github_token" {
+  description = "OAuth token for GitHub access"
+  type        = string
+  sensitive   = true
+}
